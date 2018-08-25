d5df9ae0bc060e37e0860c1c8c9a1f778c421c34
lovecrazy <630140621@qq.com>
2018-08-25 23:29:12 +0800

    增加发布订阅和事件机制,但是有缺陷,等待完善

---
+++ patterns/Observer/Event.class.js
@@ -0,0 +1,80 @@
+/**
+ * 事件机制
+ */
+
+// 使用 node 原生模块实现
+const EventEmitter = require('events')
+
+class Shop extends EventEmitter {
+    constructor() {
+        super()
+    }
+}
+
+class User {
+    constructor(name) {
+        this.name = name
+    }
+
+    buy(product) {
+        console.log(`我是${this.name}, ${product} 降价了, 快去买啊`)
+    }
+}
+
+let zhangsan = new User('张三')
+let lisi = new User('李四')
+
+let shop = new Shop()
+// 注册事件
+shop.on('promotion', zhangsan.buy.bind(zhangsan))
+shop.on('promotion', lisi.buy.bind(lisi))
+// 触发事件
+shop.emit('promotion', '苹果')
+
+// 上面的视线中, shop 同时监听和发出了 'promotion' 事件, 虽然监听的事件中可以传入用户类的方法, 但是逻辑上还是有点问题
+// 理论上应该是 shop 发出一个促销事件, user 监听促销事件, 并做出相应
+
+console.log('--------------华丽的分割线----------------')
+
+// 直接使用原生模块暂时没有想到解决办法,这里先用一个 中间人 来进行事件的发布和监听
+class Mediator extends EventEmitter {
+    constructor() {
+        super()
+    }
+}
+
+let mediator = new Mediator()
+class Shop1 {
+    emit(eventName, ...args) {
+        mediator.emit(eventName, ...args)
+    }
+}
+
+class User1 {
+    constructor(name) {
+        this.name = name
+
+        // 绑定使用了 this 的方法, 学自 React
+        // this.buy.bind(this)
+    }
+
+    on(eventName, fn, ...args) {
+        mediator.on(eventName, fn, ...args)
+    }
+
+    buy(product) {
+        console.log(`我是 ${this.name}, ${product} 促销啦, 快去买啊`)
+    }
+}
+
+let wangwu = new User1('王五')
+let xiaohong = new User1('小红')
+wangwu.on('promotion', wangwu.buy.bind(wangwu))
+xiaohong.on('promotion', xiaohong.buy.bind(xiaohong))
+xiaohong.on('promotion', xiaohong.buy.bind(xiaohong))
+let shop1 = new Shop1()
+shop1.emit('promotion', '香蕉')
+
+// 使用了一个中间人来进行事件的发布和监听, 可以在一个类中发布事件, 在另一个类中监听事件.
+// 最好不要这么使用, 感觉有隐藏的风险
+// 推荐使用第一种方法 或者 发布订阅
---
+++ patterns/Observer/Observer.class.js
@@ -0,0 +1,129 @@
+/**
+ * 简单观察者模式
+ * 
+ * 用户观察商店物品的促销情况
+ */
+class PubSub {
+    constructor() {
+        this.topic = {}
+    }
+
+    // 发布
+    publish(eventName, ...args) {
+        if (!this.topic[eventName]) {
+            throw new Error('没有这个事件')
+        }
+
+        for (let each of this.topic[eventName]) {
+            each.fn.apply(each.fn, args)
+        }
+    }
+
+    // 订阅
+    subscribe(eventName, fn) {
+        if (!this.topic[eventName]) this.topic[eventName] = []
+
+        let id = this.__getID()
+        this.topic[eventName].push({
+            id: id,
+            fn: fn
+        })
+
+        return id
+    }
+
+    // 退订
+    unsubscribe(eventName, id) {
+        this.topic[eventName] = this.topic[eventName].filter(each => {
+            if (each.id !== id) return each
+        })
+    }
+
+    // 获取唯一 id
+    __getID() {
+        return new Date().getTime() + Math.round(Math.random() * 1000000000000000000)
+    }
+}
+
+class User {
+    constructor(name) {
+        this.name = name
+    }
+
+    buy(product) {
+        console.log(`我是 ${this.name}, ${product} 促销啦, 快去买啊`)
+    }
+}
+
+// class Shop {
+//     promote(product) {
+//         console.log(`${product} 进行促销活动`)
+//     }
+// }
+
+
+// let shop = new Shop()
+let xiaohong = new User('小红')
+let xiaolan = new User('小兰')
+let xiaofang = new User('小芳')
+let zhangsan = new User('张三')
+
+let pubSub = new PubSub()
+pubSub.subscribe('promotion', xiaohong.buy.bind(xiaohong))
+pubSub.subscribe('promotion', xiaolan.buy.bind(xiaolan))
+pubSub.subscribe('promotion', xiaofang.buy.bind(xiaofang))
+let id1 = pubSub.subscribe('promotion', zhangsan.buy.bind(zhangsan))
+
+pubSub.publish('promotion', '苹果')
+
+// 退订
+pubSub.unsubscribe('promotion', id1)
+pubSub.publish('promotion', '香蕉')
+
+
+
+console.log('--------------华丽的分割线----------------')
+// 上述是通过第三个类来统一管理订阅和发布功能的
+// 逻辑上应使用 user 来订阅 shop, 然后 shop 有商品促销的时候发布
+// 参考 Event.class.js 中的做法, 将 PubSub 类的同一个实例聚集在 User 和 Shop 类中可以实现, 但是这又导致了紧耦合, 具体使用此方法时有待考虑
+class User1 {
+    constructor(name, pubSub) {
+        this.name = name
+        this.pubSub = pubSub
+    }
+
+    buy(product) {
+        console.log(`我是 ${this.name}, ${product} 促销啦, 快去买啊`)
+    }
+
+    subscribe(eventName, fn) {
+        return this.pubSub.subscribe(eventName, fn)
+    }
+}
+
+class Shop1 {
+    constructor(pubSub) {
+        this.pubSub = pubSub
+    }
+
+    promote(product) {
+        console.log(`${product} 进行促销活动`)
+        // 促销的同时发布(即通知 user)
+        this.pubSub.publish('promotion', product)
+    }
+}
+
+
+const pubSub1 = new PubSub()
+let liu = new User1('刘', pubSub1)
+let lu = new User1('卢', pubSub1)
+let zhang = new User1('张', pubSub1)
+let shop1 = new Shop1(pubSub1)
+
+liu.subscribe('promotion', liu.buy.bind(liu))
+lu.subscribe('promotion', lu.buy.bind(lu))
+zhang.subscribe('promotion', zhang.buy.bind(zhang))
+shop1.promote('苹果')
+
+// 事件被封装在了 Shop1 类中, 调用了 Shop1 类中的某些方法会自动发布一些事件, 只要和 传入 Shop1 中 pubSub 这个实例耦合均可对这些事件进行订阅
+// 缺陷~~~耦合严重
